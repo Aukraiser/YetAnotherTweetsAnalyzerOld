261d6a1d6668124148b5284b86394adc535be13f
Steven Yung <stevenyung@MacBook-Pro-de-Steven.local>
2015-11-26 10:51:23 +0100

    normalization + TODO

--- TwitterConnect/littleApp.php
+++ TwitterConnect/littleApp.php
@@ -4,6 +4,30 @@ require_once('TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 
+function display_result($string)
+{
+  foreach($string as $items)
+  {
+      echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+      echo "Tweet: ". $items['text']."<br />";
+      echo "Tweeted by: ". $items['user']['name']."<br />";
+      echo "Screen name: ". $items['user']['screen_name']."<br />";
+      echo "Followers: ". $items['user']['followers_count']."<br />";
+      echo "Friends: ". $items['user']['friends_count']."<br />";
+      echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+  }
+}
+
+function check_error($string)
+{
+  if($string["errors"][0]["message"] != "")
+  {
+      echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";
+      return (1);
+  }
+  return (0);
+}
+
 $settings = array(
     'oauth_access_token' => "1460769499-A0JKXMVFJ8HBopZRLCgPZqY4xisUmxv4XPHucAV",
     'oauth_access_token_secret' => "LDNfYScunuCjgtfDqAsDJjjYqnv8IGty5zdtjvWMCKwpC",
@@ -11,8 +35,10 @@ $settings = array(
     'consumer_secret' => "5Fio3wBlIQ3zwR6aqckxWjlTNnwXjHaOw3LOxOugrPjcQVhjCA"
 );
 
+/*TODO: change depending on the data to retrieve*/
 $url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 
+/*TODO: check methode de recupération*/
 $requestMethod = "GET";
 
 if (isset($_GET['user']))
@@ -24,6 +50,7 @@ else
     $user = "lilbowsc";
 }
 
+/*TODO: Remove */
 if (isset($_GET['count']))
 {
     $count = $_GET['count'];
@@ -33,27 +60,18 @@ else
     $count = 20;
 }
 
+/*TODO: change depending on the data to retrieve*/
 $getfield = "?screen_name=$user&count=$count";
 $twitter = new TwitterAPIExchange($settings);
 $string = json_decode($twitter->setGetfield($getfield)
                       ->buildOauth($url, $requestMethod)
                       ->performRequest(),$assoc = TRUE);
 
-if($string["errors"][0]["message"] != "")
+if (check_error($string))
 {
-    echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";
-    exit();
+  exit();
 }
 
-foreach($string as $items)
-{
-    echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-    echo "Tweet: ". $items['text']."<br />";
-    echo "Tweeted by: ". $items['user']['name']."<br />";
-    echo "Screen name: ". $items['user']['screen_name']."<br />";
-    echo "Followers: ". $items['user']['followers_count']."<br />";
-    echo "Friends: ". $items['user']['friends_count']."<br />";
-    echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-}
+display_result($string);
 
 ?>
